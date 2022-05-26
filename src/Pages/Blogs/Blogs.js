a492b0d62e908b90aae64ce9bda01fd512bad53d
Farhan-Abid <shahabid628@gmail.com>
2022-05-26 14:14:26 +0600

    done with blogs

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -3,29 +3,53 @@ import './Blogs.css';
 
 const Blogs = () => {
     return (
-        <div className='container'>
-            <h2 className='mb-5'>Welcome to our bLog</h2>
+        <div className='container px-12'>
+            <h2 className='mb-5 mt-5 text-center text-4xl text-primary'>Welcome to Our Blogs</h2>
             <div className='blog1'>
-                <h1>Difference between javascript and nodejs</h1>
-                <p>JavaScript is a language that runs inside web browsers as part of documents loaded by the browser. It gives behaviour to your pages (HTML gives semantic structure, CSS gives form or look and feel). However nothing ought to restrict JavaScript to run solely inside the browser. Now being an interpreted language, it needs an interpreter to run. V8 is the Google Chrome JS engine and 'node' is a front-end to it that can be used to run JavaScript scripts outside the browser. Node.js or just Node usually refers to a collection of objects and methods available to your JavaScript code when run in V8 or through the node interpreter. It is a JavaScript libray cum runtime.</p>
+                <h1 className='text-center text-3xl text-secondary mb-5 mt-5'>How will you improve the performance of a React Application?</h1>
+               <div className='text-center'>
+               <p>*Use memo and PureComponent
+                    <br />
+                   *Avoid Anonymous Functions
+                   <br />
+                   *Avoid Object Literals
+                   <br />
+                   *Use React.lazy and React.Suspense
+                   <br />
+                   *Avoid Frequent Mounting/Unmounting</p>
+               </div>
             </div>
             <br></br>
             <div className='blog2'>
-                <h1>When should you use nodejs and when should you use mongodb?</h1>
-                <p>Any project needs a programming environment and a runtime library that offers you basic programming tools/support and can compile and/or interpret your code. Nodejs is such as tool for the Javascript programming language. There are other similar tools for other languages such as Python, Java, PHP, C#, C++, Go, etc...So, if you want to write some kind of stand-alone program or server in Javascript, then you can use nodejs for it.
-                f your application needs the ability to persistently store data in a way that you can efficiently query or update it later, then you would typically use some form of database. There are dozens of popular databases. MongoDB is one such database. MariaDB, MySql, CouchDB, DynamoDB (on AWS), Postgres are examples of other databases. Different databases have different strengths (things they are best at) and different ways of using them so it's a whole different question to choose the right/best database for what you're doing.
-                </p>
+                <h1 className='text-center text-3xl text-secondary mb-5 mt-5'>What are the different ways to manage a state in a React application?</h1>
+                <div className='text-center'>
+               <p>1.Communication state forms the backbone of your React Native app without you even knowing about it. Remember when you had requested a call back to an HTTP request? That’s when you introduced the communication system in your app.
+                    <br />
+                    2.Data state covers information that your React application stores temporarily for various business functions. Supposedly, you are building a project management app. The information stored in the data state will include the following things – project names, contacts, details about the clients, etc.
+                   <br />
+                   3.Contrary to the state mentioned above in a React app, the control state does not represent the application’s environment. Instead, it refers to the state which the user has input into the app. For example, form inputs, selected items, etc. Control state is known to be more diverse and versatile when it comes to storing information.
+                   <br />
+                   4.Session state contains information about the user of the application such as user id, permissions, passwords, etc. It may also include information on how the application should work according to a particular user’s preferences.
+                   <br />
+                   5.Location state is the UTF-8 display that appears in your URL bar. In fact, the L in URL stands for Locator! One of the most interesting facts about Location state is that you can give directions to a user to parts of the application that do not have unique URLs associated with them. Also, the HTML5 History API allows you to store states separately from the specific URL.</p>
+               </div>
             </div>
             <br></br>
             <div className='blog3'>
-                <h1>Differences between sql and nosql databases.</h1>
-                <p>SQL pronounced as “S-Q-L” or as “See-Quel” is primarily called RDBMS or Relational Databases whereas NoSQL is a Non-relational or Distributed Database.Comparing SQL vs NoSQL database, SQL databases are table based databases whereas NoSQL databases can be document based, key-value pairs, graph databases.SQL databases are vertically scalable while NoSQL databases are horizontally scalable.SQL databases have a predefined schema whereas NoSQL databases use dynamic schema for unstructured data.Comparing NoSQL vs SQL performance, SQL requires specialized DB hardware for better performance while NoSQL uses commodity hardware.</p>
+                <h1 className='text-center text-3xl text-secondary mb-5 mt-5'> How does prototypical inheritance work?</h1>
+                <p>Every object with its methods and properties contains an internal and hidden property known as [[Prototype]]. The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the [[Prototype]] of an object, we use Object.getPrototypeOf and Object.setPrototypeOf. Nowadays, in modern language, it is being set using __proto__.
+
+</p>
             </div>
             <br></br>
             <div className='blog4'>
-                <h1>What is the purpose of jwt and how does it wor</h1>
-                <p>JWT authentication is a token-based stateless authentication mechanism. It is popularly used as a client-side-based stateless session, this means the server doesn’t have to completely rely on a data store (or) database to save session information.JWTs can be encrypted, but they are typically encoded and signed.  We will be focusing on Signed JWTs. The purpose of Signed JWT is not to hide the data but to ensure the authenticity of the data. And that is why it’s highly recommended to use HTTPS with Signed JWTs.
-                When it comes to API authentication and server-to-server authorization, JSON web token (JWT) is particularly a useful technology. In terms of Single Sign-On (SSO), it means that a service provider can receive trustworthy information from the authentication server. By sharing a secret key with the Identity Provider, the Service Provider can hash a part of a token it receives and compare it to the signature of the token. Now, if that result matches the signature, the SP knows that the information provided has come from the other entity possessing the key.
+                <h1 className='text-center text-3xl text-secondary mb-5 mt-5'> What is a unit test? Why should write unit tests?</h1>
+                <p>Unit tests are typically automated tests written and run by software developers to ensure that a section of an application (known as the "unit") meets its design and behaves as intended. In procedural programming, a unit could be an entire module, but it is more commonly an individual function or procedure.Modern versions of unit testing can be found in frameworks like JUnit, or testing tools like TestComplete. Look a little further and you will find SUnit, the mother of all unit testing frameworks created by Kent Beck, and a reference in chapter 5 of The Art of Software Testing . Before that, it's mostly a mystery. I asked Jerry Weinberg about his experiences with unit testing -- "We did unit testing in 1956. As far as I knew, it was always done, as long as there were computers".
+                </p>
+            </div>
+            <div className='blog4'>
+                <h1 className='text-center text-3xl text-secondary mb-5 mt-5'> What is a unit test? Why should write unit tests?</h1>
+                <p>Unit tests are typically automated tests written and run by software developers to ensure that a section of an application (known as the "unit") meets its design and behaves as intended. In procedural programming, a unit could be an entire module, but it is more commonly an individual function or procedure.Modern versions of unit testing can be found in frameworks like JUnit, or testing tools like TestComplete. Look a little further and you will find SUnit, the mother of all unit testing frameworks created by Kent Beck, and a reference in chapter 5 of The Art of Software Testing . Before that, it's mostly a mystery. I asked Jerry Weinberg about his experiences with unit testing -- "We did unit testing in 1956. As far as I knew, it was always done, as long as there were computers".
                 </p>
             </div>
         </div>
