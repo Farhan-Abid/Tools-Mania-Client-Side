bd2d8593e2a91871ec4297744b1332ff17beb055
Farhan-Abid <shahabid628@gmail.com>
2022-05-26 18:33:48 +0600

    changed slider

--- src/Pages/Home/Sweeper/Sweeper.js
+++ src/Pages/Home/Sweeper/Sweeper.js
@@ -7,9 +7,10 @@ import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
+
 // import required modules
 import { EffectCoverflow, Pagination } from "swiper";
-export default () => {
+const Sweeper= ()=> {
     return (
         <div>
             <h2 className='text-4xl mb-10 text-center text-primary uppercase font-bold mt-10'>Some of our Best Collections</h2>
@@ -27,14 +28,17 @@ export default () => {
         }}
         pagination={true}
         modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
+        className="mySwiper w-2/4"
       >
-        <SwiperSlide><img src="https://images.unsplash.com/photo-1586864387789-628af9feed72?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="https://images.unsplash.com/photo-1616321507403-9db926c914fd?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="https://images.unsplash.com/photo-1524224313114-ebd9c49dde82?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870" alt="" /></SwiperSlide>
-        <SwiperSlide><img src="https://images.unsplash.com/flagged/photo-1567400389308-a5709e30eb79?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870" alt="" /></SwiperSlide>
+        <SwiperSlide><img  src="https://images.unsplash.com/photo-1586864387789-628af9feed72?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870" alt="tools" /></SwiperSlide>
+        <SwiperSlide><img  src="https://images.unsplash.com/photo-1616321507403-9db926c914fd?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870" alt="tools" /></SwiperSlide>
+        <SwiperSlide><img  src="https://images.unsplash.com/photo-1524224313114-ebd9c49dde82?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870" alt="tools" /></SwiperSlide>
+        <SwiperSlide><img src="https://images.unsplash.com/flagged/photo-1567400389308-a5709e30eb79?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870" alt="tools" /></SwiperSlide>
+        
         ...
       </Swiper>
       </div>
     );
   };
+
+  export default Sweeper;
