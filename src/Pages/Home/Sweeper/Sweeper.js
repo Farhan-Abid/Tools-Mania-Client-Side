30b35e1a254deb7eba8b05c61e18af4de8e76bf3
Farhan-Abid <shahabid628@gmail.com>
2022-05-26 16:23:36 +0600

    edited sweeper

--- src/Pages/Home/Sweeper/Sweeper.js
+++ src/Pages/Home/Sweeper/Sweeper.js
@@ -1,28 +1,33 @@
-import React from 'react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 
+// import required modules
+import { EffectCoverflow, Pagination } from "swiper";
 export default () => {
     return (
         <div>
             <h2 className='text-4xl mb-10 text-center text-primary uppercase font-bold mt-10'>Some of our Best Collections</h2>
-      <Swiper
-        // install Swiper modules
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
+            <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
       >
         <SwiperSlide><img src="https://images.unsplash.com/photo-1586864387789-628af9feed72?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870" alt="" /></SwiperSlide>
         <SwiperSlide><img src="https://images.unsplash.com/photo-1616321507403-9db926c914fd?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870" alt="" /></SwiperSlide>
